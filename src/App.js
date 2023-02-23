e9e3deeb0a9c0c8e167a7147bdb569d426ef1a06
Claire Chabas <claire.chabas@gmail.com>
2023-02-23 21:42:11 +0700

    chore: setting react app + firebase config + signIn/signOut logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBlru3P7s47TlBOoMYys-EHPK1CIlRDEpg',
+  authDomain: 'realtime-chat-room-with-react.firebaseapp.com',
+  projectId: 'realtime-chat-room-with-react',
+  storageBucket: 'realtime-chat-room-with-react.appspot.com',
+  messagingSenderId: '789919013928',
+  appId: '1:789919013928:web:bfdf26349a90b5981ee91d',
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function App() {
+  /* `user` provided by `useAuthState` have 2 possible values:
+   *  => signed in: `user` is an object
+   *  => signed out: `user` is null
+   */
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
-  );
+  )
 }
 
-export default App;
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>
+}
+
+function signOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {}
+
+export default App
